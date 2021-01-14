6de8a8c31800544979e435c8815de0abbd5a34a7
annamola <annamola12345@gmail.com>
2021-01-14 17:31:32 -0600

    3 pages

--- js/main.js
+++ js/main.js
@@ -101,19 +101,6 @@ jQuery(document).ready(function($) {
     	    e.preventDefault()
     	    mySwiper.swipeNext()
         })
-        
-        // var swiper = new Swiper('.swiper-container', {
-        //     // Enable lazy loading
-        //     lazy: true,
-        //     pagination: {
-        //       el: '.swiper-pagination',
-        //       clickable: true,
-        //     },
-        //     navigation: {
-        //       nextEl: '.swiper-button-next',
-        //       prevEl: '.swiper-button-prev',
-        //     },
-        // },
 
 	/************** SlideJS *********************/
     	$('.project-slider').slidesjs({
