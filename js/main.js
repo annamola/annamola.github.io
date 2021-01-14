ee51143fccf74499b4e53d327e1a38faea0bce04
annamola <annamola12345@gmail.com>
2021-01-14 17:05:30 -0600

    swiper

--- js/main.js
+++ js/main.js
@@ -100,9 +100,20 @@ jQuery(document).ready(function($) {
     	  $('.arrow-right').on('click', function(e){
     	    e.preventDefault()
     	    mySwiper.swipeNext()
-    	})
-
-
+        })
+        
+        // var swiper = new Swiper('.swiper-container', {
+        //     // Enable lazy loading
+        //     lazy: true,
+        //     pagination: {
+        //       el: '.swiper-pagination',
+        //       clickable: true,
+        //     },
+        //     navigation: {
+        //       nextEl: '.swiper-button-next',
+        //       prevEl: '.swiper-button-prev',
+        //     },
+        // },
 
 	/************** SlideJS *********************/
     	$('.project-slider').slidesjs({
