c384adf2a295d2dae6c8bcbf080c313e1da902cb
annamola <annamola12345@gmail.com>
2021-01-16 01:59:22 -0600

    fix transition on resize

--- js/app.js
+++ js/app.js
@@ -23,24 +23,3 @@ const navSlide = () => {
     
 }
 navSlide();
-
-/************** Superfish (DropDown Menu) *********************/
-
-jQuery(document).ready(function($) {
-    function initSuperFish(){
-            
-        $(".nav-links").superfish({
-             delay:  50,
-             autoArrows: true,
-             animation:   {opacity:'show'}
-             //cssArrows: true
-        });
-        
-    }
-    
-    initSuperFish();
-    
-    $('.sub-menu').addClass('animated fadeInRight');
-    
-})
-
