bc8aa7456198bf2359725b933eb5c329af49fa8d
annamola <annamola12345@gmail.com>
2021-07-15 21:34:41 -0400

    importing icons

--- src/AnimatedLogo/AnimatedLogo.tsx
+++ src/AnimatedLogo/AnimatedLogo.tsx
@@ -1,60 +1,6 @@
 import React from "react"; // importing FunctionComponent
 
 const AnimatedLogo: React.FunctionComponent = () => {
-    consoleText(["anna mola"], "text", ["black", "tomato"]);
-
-    function consoleText(words: string[], id: string, colors: string[]) {
-        if (colors === undefined) colors = ["#fff"];
-        let visible = true;
-        const con = document.getElementById("console");
-        let letterCount = 1;
-        let x = 1;
-        let waiting = false;
-        const target = document.getElementById(id);
-        if (target && con) {
-            target.setAttribute("style", "color:" + colors[0]);
-            window.setInterval(function () {
-                if (letterCount === 0 && waiting === false) {
-                    waiting = true;
-                    target.innerHTML = words[0].substring(0, letterCount);
-                    window.setTimeout(function () {
-                        let usedColor = colors.shift()!;
-                        colors.push(usedColor);
-                        let usedWord = words.shift()!;
-                        words.push(usedWord);
-                        x = 1;
-                        target.setAttribute("style", "color:" + colors[0]);
-                        letterCount += x;
-                        waiting = false;
-                    }, 1000);
-                } else if (
-                    letterCount === words[0].length + 1 &&
-                    waiting === false
-                ) {
-                    waiting = true;
-                    window.setTimeout(function () {
-                        x = -1;
-                        letterCount += x;
-                        waiting = false;
-                    }, 6000);
-                } else if (waiting === false) {
-                    target.innerHTML = words[0].substring(0, letterCount);
-                    letterCount += x;
-                }
-            }, 130);
-
-            window.setInterval(function () {
-                if (visible === true) {
-                    con.className = "console-underscore hidden";
-                    visible = false;
-                } else {
-                    con.className = "console-underscore";
-                    visible = true;
-                }
-            }, 600);
-        }
-    }
-
     return (
         <div className="logo-container">
             <div className="console-container">
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,13 @@
 import { Grid, Hidden, makeStyles } from "@material-ui/core";
-import React from "react"; // importing FunctionComponent
+import React from "react";
 import "./Header.css";
 import AnimatedLogo from "../AnimatedLogo/AnimatedLogo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithubSquare,
+    faInstagramSquare,
+    faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const useStyles = makeStyles({
     root: {
@@ -12,6 +18,26 @@ const useStyles = makeStyles({
     menu: {
         backgroundColor: "red",
         textAlign: "center",
+    },
+    socialsContainer: {
+        display: "flex",
+        alignItems: "center",
+    },
+    socials: {
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        columnGap: "20px",
+    },
+    icon: {
+        color: "black",
+        fontSize: 35,
+        transition: "color 0.2s ease-in-out",
+        "&:hover": {
+            color: "tomato",
+            transition: "color 0.2s ease-in-out",
+        },
     },
 });
 
@@ -33,12 +59,37 @@ const Header: React.FunctionComponent = () => {
                     <p>contact</p>
                 </Grid>
             </Hidden>
-            <Grid item xs={3} sm={2} md={3}>
+            <Grid
+                item
+                xs={3}
+                sm={2}
+                md={3}
+                className={classes.socialsContainer}
+            >
                 <Hidden mdUp>
                     <p>hamburger</p>
                 </Hidden>
                 <Hidden smDown>
-                    <p>social</p>
+                    <div className={classes.socials}>
+                        <a href="https://www.instagram.com/moladesk/">
+                            <FontAwesomeIcon
+                                icon={faInstagramSquare}
+                                className={classes.icon}
+                            />
+                        </a>
+                        <a href="https://www.linkedin.com/in/anna-mola-b90581104/">
+                            <FontAwesomeIcon
+                                icon={faLinkedin}
+                                className={classes.icon}
+                            />
+                        </a>
+                        <a href="https://github.com/annamola/">
+                            <FontAwesomeIcon
+                                icon={faGithubSquare}
+                                className={classes.icon}
+                            />
+                        </a>
+                    </div>
                 </Hidden>
             </Grid>
         </Grid>
